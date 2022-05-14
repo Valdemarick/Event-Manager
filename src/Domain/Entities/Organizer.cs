0961230512f341e03ae34cbad80543fc772a27a6
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-14 21:32:29 +0300

    created database models

---
+++ src/Domain/Entities/Organizer.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Organizer : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public ICollection<Event> Events { get; set; } = null!;
+    }
+}
