03e42d1a41bfa49517d04e1b9d9e0871c8fef715
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-14 21:35:23 +0300

    created configuration of database models

---
+++ src/Infastructure/Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infastructure.Persistence.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).UseIdentityAlwaysColumn().ValueGeneratedOnAdd();
+
+            builder.Property(e => e.Date).IsRequired();
+
+            builder.Property(e => e.Description).HasMaxLength(300);
+        }
+    }
+}
---
+++ src/Infastructure/Persistence/Configurations/OrganizeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infastructure.Persistence.Configurations
+{
+    public class OrganizeConfiguration : IEntityTypeConfiguration<Organizer>
+    {
+        public void Configure(EntityTypeBuilder<Organizer> builder)
+        {
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Id).UseIdentityAlwaysColumn().ValueGeneratedOnAdd();
+
+            builder.Property(o => o.Name).HasMaxLength(50).IsRequired();
+            builder.HasIndex(o => o.Id).IsUnique();
+        }
+    }
+}
