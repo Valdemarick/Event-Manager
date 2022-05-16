c3442510d6703d85ffce0da0b68aebb044e8632c
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-16 11:03:42 +0300

    created and implemented a repository for the 'Place' entity

---
+++ src/Infastructure/Persistence/Repositories/PlaceRepository.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Interfaces.Contexts;
+using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Infastructure.Persistence.Repositories
+{
+    public class PlaceRepository : GenericRepository<Place>, IPlaceRepository
+    {
+        public PlaceRepository(IApplicationDbContext context) : base(context) { }
+    }
+}
