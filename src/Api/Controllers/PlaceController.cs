f63082490f4be58afef855d7b0c33e0d51451e9c
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-16 16:15:27 +0300

    created a controller

---
+++ src/Api/Controllers/PlaceController.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/places")]
+    [ApiController]
+    public class PlaceController : ControllerBase
+    {
+        private readonly IPlaceService _service;
+
+        public PlaceController(IPlaceService service) => _service = service;
+    }
+}
