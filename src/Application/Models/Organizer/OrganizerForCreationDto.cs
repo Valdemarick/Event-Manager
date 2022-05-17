df43665c6c0bbe02576a2e400361794beb8f80cd
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-17 17:41:10 +0300

    created DTOs of Organizer entity

---
+++ src/Application/Models/Organizer/OrganizerForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.Organizer
+{
+    public class OrganizerForCreationDto
+    {
+        [Required(ErrorMessage = "Name is a required field")]
+        public string Name { get; set; } = null!;
+    }
+}
