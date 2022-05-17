2c247e9f96ac564de0850865fd8b0dc07e5fbbb6
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-17 16:36:19 +0300

    created a base service

---
+++ src/Infastructure/Services/Common/BaseService.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace Infastructure.Services.Common
+{
+    public abstract class BaseService
+    {
+        protected readonly IMapper Mapper;
+
+        public BaseService(IMapper mapper) => Mapper = mapper;
+    }
+}
