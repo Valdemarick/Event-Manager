3f632f0f4137ac9703dbe99d58f86288fffeed8b
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-17 15:17:50 +0300

    created a fake service

---
+++ tests/Tests/Mocks/FakePlaceService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common.Interfaces.Services;
+using Moq;
+
+namespace Tests.Mocks
+{
+    public class FakePlaceService
+    {
+        public Mock<IPlaceService> Mock;
+        public IPlaceService Service;
+
+        public FakePlaceService()
+        {
+            Mock = new Mock<IPlaceService>();
+            Service = Mock.Object;
+        }
+    }
+}
