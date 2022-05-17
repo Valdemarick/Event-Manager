5611240824a6645fc12521cb1c683a8c9a755d08
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-17 16:37:37 +0300

    added an inheritance from the base service. added a calling of the base constructor

--- src/Infastructure/Services/PlaceService.cs
+++ src/Infastructure/Services/PlaceService.cs
@@ -3,42 +3,38 @@ using Application.Common.Interfaces.Services;
 using Application.Models.Place;
 using AutoMapper;
 using Domain.Entities;
+using Infastructure.Services.Common;
 
 namespace Infastructure.Services
 {
-    public class PlaceService : IPlaceService
+    public class PlaceService : BaseService, IPlaceService
     {
         private readonly IPlaceRepository _repository;
-        private readonly IMapper _mapper;
 
-        public PlaceService(IPlaceRepository repository, IMapper mapper)
-        {
-            _repository = repository;
-            _mapper = mapper;
-        }
+        public PlaceService(IPlaceRepository repository, IMapper mapper) : base(mapper) => _repository = repository;
 
         public async Task<IEnumerable<PlaceDto>> GetAllAsync()
         {
             var places = await _repository.GetAllAsync();
-            return _mapper.Map<IEnumerable<PlaceDto>>(places);
+            return Mapper.Map<IEnumerable<PlaceDto>>(places);
         }
 
         public async Task<PlaceDto> GetByIdAsync(int id)
         {
             var place = await _repository.GetByIdAsync(id);
-            return _mapper.Map<PlaceDto>(place);
+            return Mapper.Map<PlaceDto>(place);
         }
 
         public async Task<PlaceDto> CreateAsync(PlaceForCreationDto place)
         {
-            var entity = _mapper.Map<Place>(place);
+            var entity = Mapper.Map<Place>(place);
             var created = await _repository.CreateAsync(entity);
-            return _mapper.Map<PlaceDto>(created);
+            return Mapper.Map<PlaceDto>(created);
         }
 
         public async Task UpdateAsync(PlaceForUpdateDto placeForUpdateDto)
         {
-            var entity = _mapper.Map<Place>(placeForUpdateDto);
+            var entity = Mapper.Map<Place>(placeForUpdateDto);
             await _repository.UpdateAsync(entity);
         }
 
