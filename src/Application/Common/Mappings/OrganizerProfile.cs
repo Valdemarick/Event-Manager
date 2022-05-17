cfb5c4b3b6f969b0ac577e628060e35727886a59
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-17 17:40:14 +0300

    created mapping rules

---
+++ src/Application/Common/Mappings/OrganizerProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.Models.Organizer;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Mappings
+{
+    public class OrganizerProfile : Profile
+    {
+        public OrganizerProfile()
+        {
+            CreateMap<Organizer, OrganizerDto>();
+
+            CreateMap<OrganizerForCreationDto, Organizer>();
+
+            CreateMap<OrganizerForUpdateDto, Organizer>();
+        }
+    }
+}
