2b84f44c0f164427e33f4313b2030ac41ff2c3b1
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-17 22:47:33 +0300

    created a controller and implemented CRUD operations

---
+++ src/Api/Controllers/OrganizerController.cs
@@ -0,0 +1,66 @@
+﻿using Application.Common.Interfaces.Services;
+using Application.Models.Organizer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/organizers")]
+    [ApiController]
+    public class OrganizerController : ControllerBase
+    {
+        private readonly IOrganizerService _service;
+
+        public OrganizerController(IOrganizerService service) => _service = service;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var organizers = await _service.GetAllAsync();
+            return Ok(organizers);
+        }
+
+        [HttpGet("{id}")]
+        [ActionName(nameof(GetByIdAsync))]
+        public async Task<IActionResult> GetByIdAsync([FromRoute] int id)
+        {
+            var organizer = await _service.GetByIdAsync(id);
+            if (organizer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(organizer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] OrganizerForCreationDto organizerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(organizerDto);
+            }
+
+            var created = await _service.CreateAsync(organizerDto);
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = created.Id }, created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody] OrganizerForUpdateDto organizerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(organizerDto);
+            }
+
+            await _service.UpdateAsync(organizerDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        {
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
