7e53b72aeec3ea048dc22db4d51eaf2a8781c164
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-18 18:55:09 +0300

    created and implemented a repository

---
+++ src/Infastructure/Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces.Contexts;
+using Application.Common.Interfaces.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infastructure.Persistence.Repositories
+{
+    public class EventRepository : GenericRepository<Event>, IEventRepository
+    {
+        public EventRepository(IApplicationDbContext context) : base(context) { }
+
+        public override async Task<IEnumerable<Event>> GetAllAsync() =>
+            await Context.Set<Event>()
+            .Include(e => e.Organizer)
+            .Include(e => e.Place)
+            .AsNoTracking()
+            .ToListAsync();
+
+        public override async Task<Event?> GetByIdAsync(int id) =>
+            await Context.Set<Event>()
+            .Include(e => e.Organizer)
+            .Include(e => e.Place)
+            .FirstOrDefaultAsync();
+    }
+}
