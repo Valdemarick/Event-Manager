7d614f0912d5cdaf055f2dd5a5ce63cf7b392afa
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-18 18:52:48 +0300

    created DTOs for db entity 'Event'

---
+++ src/Application/Models/Event/EventDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Models.Event
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public string OrganizerName { get; set; } = null!;
+        public string PlaceName { get; set; } = null!;
+    }
+}
---
+++ src/Application/Models/Event/EventForUpdateDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.Event
+{
+    public class EventForUpdateDto
+    {
+        [Required(ErrorMessage = "Id is a required field")]
+        public int Id { get; set; }
+
+        [MaxLength(300, ErrorMessage = "Max length of the Description field cannot exceed 300 characters")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Date is a required field")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "OrganizerId is a required field")]
+        public int OrganizerId { get; set; }
+
+        [Required(ErrorMessage = "PlaceId is a required field")]
+        public int PlaceId { get; set; }
+    }
+}
