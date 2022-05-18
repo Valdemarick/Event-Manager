c551bf5fb30d830d5e50b53008c5bf09d14c7f9a
Valdemarick <korolev.vadim.2017@mail.ru>
2022-05-18 18:54:33 +0300

    created mapping rules

---
+++ src/Application/Common/Mappings/EventProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Models.Event;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Mappings
+{
+    public class EventProfile : Profile
+    {
+        public EventProfile()
+        {
+            CreateMap<Event, EventDto>()
+                .ForMember(dest => dest.OrganizerName, opt => opt.MapFrom(src => src.Organizer.Name))
+                .ForMember(dest => dest.PlaceName, opt => opt.MapFrom(src => src.Place.Name));
+
+            CreateMap<EventForCreationDto, Event>();
+
+            CreateMap<EventForUpdateDto, Event>();
+        }
+    }
+}
